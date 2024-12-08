82106af11c97daaf9b0c9d90c9a22ebe7e3e077f
norel_2b285at <norel_2b285at@NOR11>
2024-12-08 11:06:25 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    var menu = document.getElementById('menu'); if (menu.style.left === '-200px')
+    { menu.style.left = '0'; } else { menu.style.left = '-200px'; }
+});
