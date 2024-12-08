6e733cced45066c0eb4bff0d682ef06b9046fb29
norel_2b285at <norel_2b285at@NOR11>
2024-12-08 12:02:34 +0100

    Added the models for : Societe, Compte , journal

---
+++ Models/Compte.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Compta.Models
+{
+    public enum CompteType
+    {
+        Debiteur,
+        Crediteur
+    }
+    public class Compte
+    {
+        public string Id { get; set; }
+        public int Numero { get; set; }
+        public string Nom{ get; set;}
+       // public string Type { get; set; }
+        public CompteType Type {  get; set;}
+        public bool est_un_group { get; set;}
+        public string ParentId { get; set;}
+        public Compte Parent { get; set; }
+    }
+}
